adce4f187ffeacd02a5bd6612ebfe52c78680566
Phil Plueckthun <phil@plckthn.me>
2017-03-26 20:49:32 +0100

    Add new sprites instead of layer svgs

--- components/moving-train/index.js
+++ components/moving-train/index.js
@@ -24,68 +24,81 @@ const Wrapper = styled(Layer)`
   overflow: hidden;
 `
 
-const Sky = styled(Layer)`
-  background: url('/static/sky.svg');
-  background-repeat: repeat-x;
-  background-position: top center;
-  bottom: 95px;
-  left: 0;
+const Cloud = styled.div`
+  position: absolute;
+  background: url('/static/cloud.svg');
+  background-repeat: no-repeat;
+  background-size: contain;
+  height: 100px;
+  width: 150px;
+  top: ${props => props.top};
+  left: ${props => props.left};
+  transform: scale(${props => props.scale});
 `
 
-const Skyline = styled(Layer)`
-  bottom: 18px;
+const Skyline = styled.div`
+  position: absolute;
+  bottom: 35px;
   left: 0;
   right: 0;
-  height: 315px;
-  background: url('/static/skyline.svg');
-  background-repeat: repeat-x;
-  background-position: top center;
-  background-size: cover;
-  animation: 30s linear 0s ${translateX} both;
+  width: 100%;
+  height: 300px;
+  background: url('/static/skyline.svg'), url('/static/skyline.min.svg');
+  background-repeat: no-repeat, no-repeat;
+  background-position: 20% 50%, 105% 50%;
+  background-size: auto 300px, auto 300px;
+  /* animation: 30s linear 0s ${translateX} both; */
 `
 
 const Landscape = styled.div`
   position: absolute;
   left: 0;
   right: 0;
-  bottom: 100px;
-  height: 315px;
+  width: 100%;
+  bottom: 80px;
+  height: 150px;
 
   background: url('/static/landscape.svg');
   background-repeat: repeat-x;
-  background-position: top center;
-  background-size: contain;
-  animation: ${translateX} 100s infinite linear both;
+  background-position: bottom center;
+  background-size: auto 150px;
+  /* animation: ${translateX} 100s infinite linear both; */
 `
 
 const Bridge = styled.div`
   position: absolute;
   left: 0;
   right: 0;
-  bottom: 0;
-  height: 315px;
+  bottom: -5px;
+  width: 100%;
+  height: 115px;
+
   background: url('/static/bridge.svg');
   background-repeat: repeat-x;
-  background-position: top center;
   background-size: contain;
-  animation: ${translateX} 7s infinite linear both;
+  /* animation: ${translateX} 7s infinite linear both; */
 `
 
 const Train = styled.div`
   position: absolute;
-  bottom: -88px;
-  height: 315px;
-  width: 1500px;
-  left: 555px;
+  left: 35%;
+  right: 0;
+  bottom: 110px;
+  width: 100%;
+  height: 45px;
+
   background: url('/static/train.svg');
   background-repeat: no-repeat;
-  background-position: top center;
-  background-size: cover;
+  background-position: 0% center;
+  background-size: auto 45px;
 `
 
 const MovingTrain = () => (
   <Wrapper>
-    <Sky />
+    <Cloud top="10%" left="10%" scale="1.8"/>
+    <Cloud top="25%" left="5%" scale="1"/>
+    <Cloud top="13%" left="80%" scale="2"/>
+
     <Landscape />
     <Skyline />
     <Bridge />
